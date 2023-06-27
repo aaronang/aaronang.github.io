67d7402591dffc1edd3d661dbc12c086b53695d3
Aaron Ang <awz.ang@gmail.com>
2023-06-26 23:40:40 -0700

    Base with fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Mona Sans', ...defaultTheme.fontFamily.sans],
+        'heading': ['Hubot Sans', ...defaultTheme.fontFamily.sans],
+      }
+    },
   },
   plugins: [],
 }
